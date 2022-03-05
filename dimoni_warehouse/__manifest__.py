0ff764dd6953f1adbe8ac395e51ad7d2c563526f
bilbonet <jesus@bilbonet.net>
2022-03-05 17:35:18 +0100

    [MIG] Change versión number to 13

--- dimoni_warehouse/__manifest__.py
+++ dimoni_warehouse/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Dimoni Warehouse Operations",
     "summary": "Dimoni warehouse operations from Odoo sales orders",
-    "version": "12.0.2.0.0",
+    "version": "13.0.2.0.0",
     "author": "Jesus Ramiro",
     "license": "AGPL-3",
     "category": "Connector",
